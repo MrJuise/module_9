7f0bc4935bf9e3e9eb8fd00960165cf116649a5e
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-09-04 22:17:02 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,20 @@
+'''
+Задача "Вызов разом":
+Напишите функцию apply_all_func(int_list, *functions), которая принимает параметры:
+
+    int_list - список из чисел (int, float)
+    *functions - неограниченное кол-во функций (которые применимы к спискам, состоящим из чисел)
+
+Эта функция должна:
+
+    Вызвать каждую функцию к переданному списку int_list
+    Возвращать словарь, где ключом будет название вызванной функции, а значением - её результат работы со списком int_list.
+'''
+def apply_all_func(int_list, *functions):
+    result = {}
+    for function in functions:
+        result.update({function.__name__ : function(int_list)})
+    return result
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
