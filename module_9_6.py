82b8ecc1a4519558ed3180a51988479cb3be1fe3
MrJuise <126503964+MrJuise@users.noreply.github.com>
2024-09-09 12:54:57 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -6,17 +6,11 @@
 '''
 
 def all_variants(text):
-    len_txt = len(text)
-
-
-    for start in range(len_txt):
-
-
-        for end in range(start + 1, len_txt + 1):
-
-
-            yield text[start:end]
-
+    for length in range(1, len(text) + 1):
+        for start in range(len(text)):
+            if start + length <= len(text):
+                yield text[start:start + length]
 a = all_variants("abc")
 for i in a:
     print(i)
+
